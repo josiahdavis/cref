2ff3540fce23a6b467af8a9438d28567098bcf6e
Josiah Davis <josiah.j.davis@gmail.com>
2024-06-18 20:42:24 -0500

    Pointer basics to reference

--- reference/main.c
+++ reference/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void swap_wrong(int x, int y){
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+void swap_correct(int *px, int *py){
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
 int main()
 {
 
@@ -103,4 +117,41 @@ int main()
     }
     printf("\nIn the following string: %s there are \n", abcs);
     printf("a_count=%d, b_count=%d, c_count=%d\n", a_count, b_count, c_count);
+
+    /* --------------
+    -- Pointers    --
+    ---------------- */
+
+    // Pointer basics
+    // * is the indirection operator, & is the unary operator
+
+    int x = 1, y = 2, z[10];
+    int *ip;    // Declaring the pointer, it has a type, it can only point to integers now.
+    ip = &x;    // &x is a pointer to x, so we say ip points to x
+    y = *ip;    // *ip gets the value of the thing ip is pointing to, in this case 1, so y is now 1.
+    *ip = 0;    // x is now 0;
+    ip = &z[0]; // ip is now pointing to z[0]
+
+    // Pointers and function arguments
+    // Enable a function to access and change objects *within* the function that called it
+    int x_coord = 3;
+    int y_coord = 5;
+    printf("Before swap:          x_coord=%d, y_coord=%d\n", x_coord, y_coord);
+    swap_wrong(x_coord, y_coord);
+    printf("After swap (wrong):   x_coord=%d, y_coord=%d\n", x_coord, y_coord);
+    int *pointer_x;
+    int *pointer_y;
+    pointer_x = &x_coord;
+    pointer_y = &y_coord;
+    swap_correct(pointer_x, pointer_y);
+    printf("After swap (correct): x_coord=%d, y_coord=%d\n", x_coord, y_coord);
+
+    // Pointers and arrays
+    // Close correspondence between indexing arrays and pointer arithmetic
+    int my_arr[5] = {0, 1, 2, 3, 4};
+    int *pa;
+    pa = &my_arr[0];
+    printf(" *pa     points to %d\n", *pa);
+    printf(" *(pa+1) points to %d\n", *(pa+1));
+    printf(" *(pa+2) points to %d\n", *(pa+2));
 }
